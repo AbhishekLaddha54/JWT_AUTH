65ad70962ba2784b933a1492adc4c2e05e9ae6b4
AbhishekLaddha54 <146935931+AbhishekLaddha54@users.noreply.github.com>
2024-06-30 02:06:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,79 +1,8 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const app = express();
-app.get("/api", (req, res) => {
-
-    res.json({
-  
-      message: "Hey, there! Welcome to this API service"
-  
-    });
-  
-  });
-  app.post("/api/posts", verifyToken, (req, res) => {
-
-    jwt.verify(req.token, "secretkey", (err, authData) => {
-  
-      if (err) {
-  
-        res.sendStatus(403);
-  
-      } else {
-  
-        res.json({
-  
-          message: "POST created...",
-  
-          authData
-  
-        });
-  
-      }
-  
-    });
-  
-  });
-  
-  app.post("/api/login", (req, res) => {
-
-    const user = {
-  
-      id: 1,
-  
-      username: "john",
-  
-      email: "john@gmail.com"
-  
-    };
-  
-    jwt.sign({ user: user }, "secretkey", (err, token) => {
-  
-      res.json({
-  
-        token
-  
-      });
-  
-    });
-  
-  });
-  function verifyToken(req, res, next) {
-
-    const bearerHeader = req.headers["authorization"];
-  
-    if (typeof bearerHeader !== "undefined") {
-  
-      const bearerToken = bearerHeader.split(" ")[1];
-  
-      req.token = bearerToken;
-  
-      next();
-  
-    } else {
-  
-      res.sendStatus(403);
-  
-    }
-  
-  }  
-  
+module.exports = {
+  decode: require('./decode'),
+  verify: require('./verify'),
+  sign: require('./sign'),
+  JsonWebTokenError: require('./lib/JsonWebTokenError'),
+  NotBeforeError: require('./lib/NotBeforeError'),
+  TokenExpiredError: require('./lib/TokenExpiredError'),
+};
